937376cf5492ae64d10786d4de903e931777d392
Beverly Green <greenbev000@gmail.com>
2025-03-26 14:18:55 -0500

    Cleaned up logs out of controller and removed language req

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,23 +1,17 @@
 class Api::V1::MoviesController < ApplicationController
     
     def index
-        api_key = Rails.application.credentials.dig(:tmdb, :api_key) # Safely fetch key
-
-        Rails.logger.info "Using TMDb API Key: #{api_key}"
-
         conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
             faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.tmdb[:api_key]}"
             faraday.headers["Accept"] = "application/json"
         end
     
-        response = conn.get("/3/movie/top_rated", { language: "en-US", page: 1 })
-
-        Rails.logger.info "TMDb API Response Status: #{response.status}"
-        Rails.logger.info "TMDb API Response Body: #{response.body}"
+        response = conn.get("/3/movie/top_rated")
+        # { language: "en-US", page: 1 }
         json = JSON.parse(response.body, symbolize_names: true)
 
         if response.success?
-            top_movies = json[:results].first(20) # Get only the first 20 movies
+            top_movies = json[:results].first(20) 
             render json: MovieSerializer.format_movie_list(top_movies) #create serializer with required data
         else
             render json: { error: "Failed to fetch movies" }, status: :bad_request
