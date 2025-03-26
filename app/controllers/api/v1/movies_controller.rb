7e8154f85545868564f54fdf443544a71f16c729
Beverly Green <greenbev000@gmail.com>
2025-03-25 19:49:27 -0500

    Fixed API key to correct key, added index method to movie controller, added correct attributes to movie serializer

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,26 @@
 class Api::V1::MoviesController < ApplicationController
-
+    
     def index
-        movies = Movie.get_top_rated_movies #create method/data logic handling in model
-        render json: MovieSerializer.format_movie_list(movies) #create serializer with required data
+        api_key = Rails.application.credentials.dig(:tmdb, :api_key) # Safely fetch key
+
+        Rails.logger.info "Using TMDb API Key: #{api_key}"
+
+        conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+            faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.tmdb[:api_key]}"
+            faraday.headers["Accept"] = "application/json"
+        end
+    
+        response = conn.get("/3/movie/top_rated", { language: "en-US", page: 1 })
+
+        Rails.logger.info "TMDb API Response Status: #{response.status}"
+        Rails.logger.info "TMDb API Response Body: #{response.body}"
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        if response.success?
+            top_movies = json[:results].first(20) # Get only the first 20 movies
+            render json: MovieSerializer.format_movie_list(top_movies) #create serializer with required data
+        else
+            render json: { error: "Failed to fetch movies" }, status: :bad_request
+        end
     end
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,18 +2,16 @@ class MovieSerializer
     include JSONAPI::Serializer
     attributes :title, :vote_average
 
-    def self.format_movie_list(movies)
-        { data:
-            movies.map do |movie|
+    def self.format_movie_list(top_movies)
+        top_movies.map do |movie|
             {
-                id: movie.id.to_s,
-                type: "movie",
-                attributes: {
-                title: movie.title,
-                vote_average: movie.vote_average #not sure yet if that is how i call those attributes of a movie
-                }
+            id: movie[:id],
+            type: "movie",
+            attributes: {
+                title: movie[:title],
+                vote_average: movie[:vote_average]
             }
-            end
-        }
+            }
+        end
     end
 end
