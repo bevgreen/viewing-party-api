cf6551d8992486697604d7b4b6bb91897b1c68c5
Beverly Green <greenbev000@gmail.com>
2025-03-28 20:21:49 -0500

    Added test for class method in viewing party model, will add more when refactored into smaller methods

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -6,4 +6,31 @@ RSpec.describe ViewingParty, type: :model do
     it { should have_many(:viewing_party_users)}
     it { should have_many(:users)}
   end
+
+  describe "class methods" do
+    before(:each) do
+      @host = User.create!(
+        name: "Beverly",
+        username: "bgree143",
+        password: "password123"
+      )
+
+      @party = ViewingParty.create!(
+        name: "Beverly's Movie Night",
+        start_time: "2025-04-01 18:00:00",
+        end_time: "2025-04-01 21:00:00",
+        movie_id: 278,
+        movie_title: "The Shawshank Redemption",
+        host_id: @host.id
+      )
+    end
+
+    it "creates a viewing party with valid attributes" do
+      expect(@party).to be_a(ViewingParty)
+      expect(@party.name).to eq("Beverly's Movie Night")
+      expect(@party.movie_title).to eq("The Shawshank Redemption")
+      expect(@party.host_id).to eq(@host.id)
+    end
+  end
+  #MORE TESTS WHEN METHODS GET REFACTORED IN VIEWING PARTY MODEL 
 end
