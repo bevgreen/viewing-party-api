2509d9ca9989e3195ee0c36e969b9f4325bdf69a
Beverly Green <greenbev000@gmail.com>
2025-03-31 12:39:48 -0500

    Changed movie details poro to format json response into ruby object

--- app/poros/movie_details.rb
+++ app/poros/movie_details.rb
@@ -1,12 +1,24 @@
 class MovieDetails
-   
-    #MOVE TO MOVIE GATEWAY FILE 
-    private 
-
-    def self.conn 
-        Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-            faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.tmdb[:api_key]}"
-            faraday.headers["Accept"] = "application/json"
-        end
+    attr_reader :id, :title, :release_year, :vote_average, :runtime, :genres, :summary, :cast, :total_reviews, :reviews
+  
+    def initialize(movie_data, cast_data, reviews_data)
+        @id = movie_data[:id]
+        @title = movie_data[:title]
+        @release_year = movie_data[:release_date]&.split("-").first.to_i #splits release date into an array and grabs first thing(year), turns to int
+        @vote_average = movie_data[:vote_average]
+        @runtime = format_runtime(movie_data[:runtime])
+        @genres = movie_data[:genres].map { |genre| genre[:name] }
+        @summary = movie_data[:overview]
+        @cast = cast_data.map { |member| { character: member[:character], actor: member[:name] } }
+        @total_reviews = reviews_data.count
+        @reviews = reviews_data.map { |review| { author: review[:author], review: review[:content] } }
     end
+
+    private
+
+  def format_runtime(minutes)
+        hours = minutes / 60
+        mins = minutes % 60
+        "#{hours} hours, #{mins} minutes"
+  end
 end
